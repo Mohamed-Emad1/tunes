036c98daa847a8b56c32abca353d0683010a3290
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-01-29 21:53:21 +0200

    final push

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -7,34 +7,46 @@ class TuneView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Expanded(
+          AppBar(
+            elevation: 0,
+            centerTitle: true,
+            title: const Text(
+              'Flutter Tune',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            backgroundColor: const Color(0xff243139),
+          ),
+          const Expanded(
             child: Item(
                 item: ItemInfo(color: Color(0xffF44235), sound: 'note1.wav')),
           ),
-          Expanded(
+          const Expanded(
             child: Item(
                 item: ItemInfo(color: Color(0xffF89800), sound: 'note2.wav')),
           ),
-          Expanded(
+          const Expanded(
             child: Item(
                 item: ItemInfo(color: Color(0xffFEEB3A), sound: 'note3.wav')),
           ),
-          Expanded(
+          const Expanded(
             child: Item(
                 item: ItemInfo(color: Color(0xff4CAF50), sound: 'note4.wav')),
           ),
-          Expanded(
+          const Expanded(
             child: Item(
                 item: ItemInfo(color: Color(0xff2E9688), sound: 'note5.wav')),
           ),
-          Expanded(
+          const Expanded(
             child: Item(
                 item: ItemInfo(color: Color(0xff2796F3), sound: 'note6.wav')),
           ),
-          Expanded(
+          const Expanded(
             child: Item(
                 item: ItemInfo(color: Color(0xff9C27B0), sound: 'note7.wav')),
           )
